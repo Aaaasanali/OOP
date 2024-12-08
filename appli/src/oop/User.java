185fa17e73232f7377432adfd4e05c25f1990d31
Yerassyl <babayevyerasyl@gmail.com>
2024-12-09 01:31:06 +0500

    user email upd

--- appli/src/oop/User.java
+++ appli/src/oop/User.java
@@ -10,7 +10,7 @@ public class User {
     private int id;
     private String name;
     private String surname;
-    private String email1234wijrgbpowejrtbqibhwoeutbqerwwert;
+    private String email1234wijrgbpowejrthwoeutbqerwwertbqibhwoeutbqerwwerthwoeutbqerwwerthwoeutbqerwwert;
     private String password;
     private Language language;
     private Date birthdate;
