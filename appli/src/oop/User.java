0e917e76063c9ebb29b3c13b76e8e85db1fa9598
Асанали <@DESKTOP-RLPBNCP>
2024-12-09 02:30:05 +0600

    a

--- appli/src/oop/User.java
+++ appli/src/oop/User.java
@@ -10,7 +10,7 @@ public class User {
     private int id;
     private String name;
     private String surname;
-    private String email;
+    private String emaila;
     private String password;
     private Language language;
     private Date birthdate1234wijrgbpowejrtbqibhwoeutbqerwt;
