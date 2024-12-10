5d8137308bf463e4ac19041032d847fc596cf6a4
Yerassyl <babayevyerasyl@gmail.com>
2024-12-10 14:13:26 +0500

    Researcher interface

---
+++ appli/src/oop/Research/Researcher.java
@@ -0,0 +1,8 @@
+package oop.Research;
+
+public interface Researcher {
+	public void addResearchPaper();
+	public ResearchPaper getresearchPaper();
+	public int calculateHIndex();
+	public void printResearchPaper();
+}
