b84447a3256ed1d75c23cdc396cd2cb7bb0a59c0
i-nur <i-nur@Ilkham>
2024-12-18 23:44:46 +0500

    вывы

--- UniSystemOOP/src/employees/Dean.java
+++ UniSystemOOP/src/employees/Dean.java
@@ -2,16 +2,18 @@ package employees;
 
 import java.util.*;
 
+import Factories.NamedRunnable;
 import Messages.*;
 
 public class Dean extends Employee{
+	 private final List<Request> requests = Collections.synchronizedList(new ArrayList<>());
+	 private final List<Complaint> complaints = Collections.synchronizedList(new ArrayList<>());
+
 	
-	private final Vector<Request> requests = new Vector<>();
-	private final Vector<Complaint> complaints = new Vector<>();
 	
-	public Dean() {
-		
-	}
+	 public Dean(String name, String department) {
+	        super(name, department);
+	    }
 	
 	public void addRequest(Request r) {
 		this.requests.add(r);
@@ -19,4 +21,47 @@ public class Dean extends Employee{
 	public void addComplaint(Complaint c) {
 		this.complaints.add(c);
 	}
-}
+	  public List<Request> getRequests() {
+	        return Collections.unmodifiableList(requests);
+	    }
+
+	    public List<Complaint> getComplaints() {
+	        return Collections.unmodifiableList(complaints);
+	    }
+	    
+	    public String toString() {
+	        return "Dean: " + this.getName() + ", Department: " + this.getDepartment();
+	    }
+
+	    @Override
+	    public boolean equals(Object obj) {
+	        if (this == obj) return true;
+	        if (obj == null || getClass() != obj.getClass()) return false;
+	        Dean dean = (Dean) obj;
+	        return Objects.equals(this.getName(), dean.getName()) &&
+	               Objects.equals(this.getDepartment(), dean.getDepartment());
+	    }
+
+	    @Override
+	    public int hashCode() {
+	        return Objects.hash(this.getName(), this.getDepartment());
+	    }
+	    
+	    
+	    
+	    
+	    public Map<Integer, NamedRunnable> getFunctions(int startIndex){
+	    	Map<Integer, NamedRunnable> functions = new LinkedHashMap<>();
+	    	functions.put(startIndex++, new NamedRunnable(this::approveRequest,"approveRequest"));
+	    	functions.put(startIndex++, new NamedRunnable(this::rejectRequest,"rejectRequest"));
+	    	functions.put(startIndex++, new NamedRunnable(this::assignComplaint,"assignComplaint"));
+	    	functions.put(startIndex++, new NamedRunnable(this::archiveComplaint,"archiveComplaint"));
+	    	
+	    	for(Map.Entry<Integer,NameRunnable> entry : super.getFunctionsMap(startIndex).entrySet()) {
+	    		functions.put(startIndex++, entry.getValue());
+	    	}
+	    	return functions;
+	    	
+	    }
+	}
+
