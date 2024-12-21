93195729b8f70accd1da96f6f5b8a7c7b12b6c75
Yerassyl <babayevyerasyl@gmail.com>
2024-12-21 20:45:52 +0500

    Bachelor changes

--- UniSystemOOP/src/students/Bachelor.java
+++ UniSystemOOP/src/students/Bachelor.java
@@ -1,14 +1,39 @@
 package students;
 
 import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import Factories.NamedRunnable;
+import documents.DiplomaWork;
 
 public class Bachelor extends Student implements Serializable{
     
-
-    public Bachelor(String login, String password, String name, String surname) {
+	private DiplomaWork diplomaWork;
+	
+	
+	
+	
+	public Bachelor(String login, String password, String name, String surname) {
 		super(login, password, name, surname);
 
 	}
+	
+	
+	
+	
+	
+    public DiplomaWork getDiplomaWork() {
+		return diplomaWork;
+	}
+
+
+	public void setDiplomaWork(DiplomaWork diplomaWork) {
+		this.diplomaWork = diplomaWork;
+	}
+
+
+	
 
 
     
@@ -17,7 +42,26 @@ public class Bachelor extends Student implements Serializable{
    
     
     
-    
+    public Map<Integer, NamedRunnable> getFunctionsMap(int startIndex) {
+        Map<Integer, NamedRunnable> functions = new LinkedHashMap<>();
+        functions.put(startIndex++, new NamedRunnable(this::getDiplomaWork, "Get Thesis"));
+        //functions.put(startIndex++, new NamedRunnable(this::getString, "Get String"));
+        
+        for (Map.Entry<Integer, NamedRunnable> entry : super.getFunctionsMap(startIndex).entrySet()) {
+            functions.put(startIndex++, entry.getValue());
+        }
+        
+        return functions;
+    }
+
+
+
+
+
+	@Override
+	public String toString() {
+		return super.toString() + ", Thesis: " + (diplomaWork != null ? diplomaWork.toString() : "No thesis assigned");
+	}
 
     
     
