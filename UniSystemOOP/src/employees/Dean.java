97237f6cde1c7df6bc305ffa853483ae0e70144d
Yerassyl <babayevyerasyl@gmail.com>
2024-12-22 21:18:43 +0500

    Rector, Dean documentation and logs

--- UniSystemOOP/src/employees/Dean.java
+++ UniSystemOOP/src/employees/Dean.java
@@ -9,92 +9,115 @@ import documents.Complaint;
 import documents.Request;
 import utils.InputPrompt;
 
-public class Dean extends Employee implements Serializable{
+/**
+ * The Dean class represents an employee who manages student complaints and
+ * requests.
+ *
+ * <p>
+ * The class includes methods for interacting with complaints and requests,
+ * including the ability to approve or reject requests.
+ * </p>
+ */
+public class Dean extends Employee implements Serializable {
 
-    public static Vector<Complaint> complaints = new Vector<>();
-    
-    private static List<Request> approvedRequests = new ArrayList<>();
+	public static Vector<Complaint> complaints = new Vector<>();
 
+	private static List<Request> approvedRequests = new ArrayList<>();
 
-    public Dean(String login, String password) {
-        super(login, password);			
-        // TODO Auto-generated constructor stub
-    }
-    
-    public Dean(String login, String password, String name, String surname) {
-    	super(login, password, name, surname);
+	public Dean(String login, String password) {
+		super(login, password);
+		// TODO Auto-generated constructor stub
 	}
-    
-    
 
-    public static void getComplaints() {
-        if (complaints.isEmpty()) {
-            System.out.println("No complaints have been filed.");
-        } else {
-            for (Complaint complaint : complaints) {
-                System.out.println(complaint);
-            }
-        }
-    }
-    
-    public static List<Request> getApprovedRequests() {
-        return approvedRequests;
-    }
-    
-    
-    public void signRequests() {
-        List<Request> pendingRequests = Data.INSTANCE.getRequests();
+	public Dean(String login, String password, String name, String surname) {
+		super(login, password, name, surname);
+	}
 
-        if (pendingRequests.isEmpty()) {
-            System.out.println("No requests to review.");
-            return;
-        }
+	/**
+	 * Retrieves and prints all complaints filed by students.
+	 */
+	public static void getComplaints() {
+		if (complaints.isEmpty()) {
+			System.out.println("No complaints have been filed.");
+		} else {
+			for (Complaint complaint : complaints) {
+				System.out.println(complaint);
+			}
+		}
+	}
 
-        System.out.println("Pending Requests:");
-        for (int i = 0; i < pendingRequests.size(); i++) {
-            System.out.println((i + 1) + ". " + pendingRequests.get(i));
-        }
+	/**
+	 * Retrieves the list of approved requests.
+	 * 
+	 * @return A list of all approved requests.
+	 */
+	public static List<Request> getApprovedRequests() {
+		return approvedRequests;
+	}
 
-        while (true) {
-            int requestIndex = InputPrompt.promptIntInput("Enter the number of the request to approve (or type 'quit' to exit): ");
-            if (requestIndex == -1) {
-                return; // User chose to quit
-            }
+	/**
+	 * Process of student requests handling. Displays pending requests to the Dean,
+	 * allows selection of requests to approve, and forwards approved requests to
+	 * the Manager for further processing.
+	 */
+	public void signRequests() {
+		List<Request> pendingRequests = Data.INSTANCE.getRequests();
 
-            if (requestIndex < 1 || requestIndex > pendingRequests.size()) {
-                System.out.println("Invalid request number. Please try again.");
-                continue;
-            }
+		if (pendingRequests.isEmpty()) {
+			System.out.println("No requests to review.");
+			return;
+		}
 
-            // Approve the selected request
-            Request approvedRequest = pendingRequests.remove(requestIndex - 1);
-            approvedRequests.add(approvedRequest);
-//            System.out.println("Approved request: " + approvedRequest);					//To logs
+		System.out.println("Pending Requests:");
+		for (int i = 0; i < pendingRequests.size(); i++) {
+			System.out.println((i + 1) + ". " + pendingRequests.get(i));
+		}
 
-            // Forward to Manager
-            Manager.receiveRequest(approvedRequest);
+		while (true) {
+			int requestIndex = InputPrompt
+					.promptIntInput("Enter the number of the request to approve (or type 'quit' to exit): ");
+			if (requestIndex == -1) {
+				return;
+			}
 
-            // Check if there are more requests to process
-            if (pendingRequests.isEmpty()) {
-                System.out.println("No more pending requests.");
-                break;
-            }
-        }
-    }
-    
-    
+			if (requestIndex < 1 || requestIndex > pendingRequests.size()) {
+				System.out.println("Invalid request number. Please try again.");
+				continue;
+			}
 
-    public Map<Integer, NamedRunnable> getFunctionsMap(int startIndex) {
-        Map<Integer, NamedRunnable> functions = new LinkedHashMap<>();
-        // Reference the static method with the class name
-        functions.put(startIndex++, new NamedRunnable(Dean::getComplaints, "Get Complaints"));
-        functions.put(startIndex++, new NamedRunnable(this::signRequests, "Sign Request"));
+			// Approve
+			Request approvedRequest = pendingRequests.remove(requestIndex - 1);
+			approvedRequests.add(approvedRequest);
 
+			// Log
+			String logMessage = "Approved request: " + approvedRequest.toString() + " by Dean " + this.getName() + " "
+					+ this.getSurname();
+			Data.INSTANCE.addLog(logMessage);
 
-        for (Map.Entry<Integer, NamedRunnable> entry : super.getFunctionsMap(startIndex).entrySet()) {
-            functions.put(startIndex++, entry.getValue());
-        }
+			// Send to Manager
+			Manager.receiveRequest(approvedRequest);
 
-        return functions;
-    }
+			// Check for more
+			if (pendingRequests.isEmpty()) {
+				System.out.println("No more pending requests.");
+				break;
+			}
+		}
+	}
+
+	/**
+	 * The method for registering available actions in the map
+	 */
+	public Map<Integer, NamedRunnable> getFunctionsMap(int startIndex) {
+		Map<Integer, NamedRunnable> functions = new LinkedHashMap<>();
+
+		functions.put(startIndex++, new NamedRunnable(Dean::getComplaints, "Get Complaints"));
+		functions.put(startIndex++, new NamedRunnable(this::signRequests, "Sign Request"));
+
+		for (Map.Entry<Integer, NamedRunnable> entry : super.getFunctionsMap(startIndex).entrySet()) {
+			functions.put(startIndex++, entry.getValue());
+		}
+
+		return functions;
+	}
 }
--- UniSystemOOP/src/employees/Rector.java
+++ UniSystemOOP/src/employees/Rector.java
@@ -3,87 +3,103 @@ package employees;
 import java.io.Serializable;
 import java.util.*;
 
-
 import Factories.NamedRunnable;
 import database.Data;
 import documents.Request;
 import utils.InputPrompt;
 
+/**
+ * The Rector class represents an employee responsible for reviewing and signing
+ * student requests at the highest level.
+ * <p>
+ * The class includes methods for interacting with pending requests, including
+ * approving or rejecting them.
+ * </p>
+ */
 public class Rector extends Employee implements Serializable {
 
 	private static List<Request> approvedRequests = new ArrayList<>();
 
-	
 	public Rector(String login, String password) {
 		super(login, password);
-		// TODO Auto-generated constructor stub
 	}
-	
-    public Rector(String login, String password, String name, String surname) {
-    	super(login, password, name, surname);
+
+	public Rector(String login, String password, String name, String surname) {
+		super(login, password, name, surname);
 	}
-    
-    
-	
+
+	/**
+	 * Retrieves the list of requests that have been approved by the Rector.
+	 * 
+	 * @return A list of approved requests.
+	 */
 	public static List<Request> getApprovedRequests() {
-        return approvedRequests;
-    }
-	
-	
-	
+		return approvedRequests;
+	}
+
+	/**
+	 * Process of student requests handling. Displays pending requests to the
+	 * Rector, allows selection of requests to approve, and forwards approved
+	 * requests to the Manager for further processing.
+	 */
 	public void signRequests() {
-        List<Request> pendingRequests = Data.INSTANCE.getRequests();
+		List<Request> pendingRequests = Data.INSTANCE.getRequests();
 
-        if (pendingRequests.isEmpty()) {
-            System.out.println("No requests to review.");
-            return;
-        }
+		if (pendingRequests.isEmpty()) {
+			System.out.println("No requests to review.");
+			return;
+		}
 
-        System.out.println("Pending Requests:");
-        for (int i = 0; i < pendingRequests.size(); i++) {
-            System.out.println((i + 1) + ". " + pendingRequests.get(i));
-        }
+		System.out.println("Pending Requests:");
+		for (int i = 0; i < pendingRequests.size(); i++) {
+			System.out.println((i + 1) + ". " + pendingRequests.get(i));
+		}
 
-        while (true) {
-            int requestIndex = InputPrompt.promptIntInput("Enter the number of the request to approve (or type 'quit' to exit): ");
-            if (requestIndex == -1) {
-                return; // User chose to quit
-            }
+		while (true) {
+			int requestIndex = InputPrompt
+					.promptIntInput("Enter the number of the request to approve (or type 'quit' to exit): ");
+			if (requestIndex == -1) {
+				return;
+			}
 
-            if (requestIndex < 1 || requestIndex > pendingRequests.size()) {
-                System.out.println("Invalid request number. Please try again.");
-                continue;
-            }
+			if (requestIndex < 1 || requestIndex > pendingRequests.size()) {
+				System.out.println("Invalid request number. Please try again.");
+				continue;
+			}
 
-            // Approve the selected request
-            Request approvedRequest = pendingRequests.remove(requestIndex - 1);
-            approvedRequests.add(approvedRequest);
-//            System.out.println("Approved request: " + approvedRequest);			To logs
+			// Approve
+			Request approvedRequest = pendingRequests.remove(requestIndex - 1);
+			approvedRequests.add(approvedRequest);
 
-            // Forward to Manager
-            Manager.receiveRequest(approvedRequest);
+			// Log
+			String logMessage = "Approved request: " + approvedRequest.toString() + " by Rector " + this.getName() + " "
+					+ this.getSurname();
+			Data.INSTANCE.addLog(logMessage);
 
-            // Check if there are more requests to process
-            if (pendingRequests.isEmpty()) {
-                System.out.println("No more pending requests.");
-                break;
-            }
-        }
-    }
-		
+			// Send to Manager
+			Manager.receiveRequest(approvedRequest);
 
-	
+			// Check for more
+			if (pendingRequests.isEmpty()) {
+				System.out.println("No more pending requests.");
+				break;
+			}
+		}
+	}
+
+	/**
+	 * The method for registering available actions in the map
+	 */
 	public Map<Integer, NamedRunnable> getFunctionsMap(int startIndex) {
-        Map<Integer, NamedRunnable> functions = new LinkedHashMap<>();
-        // Reference the static method with the class name
-        
-        functions.put(startIndex++, new NamedRunnable(this::signRequests, "Sign Request"));
+		Map<Integer, NamedRunnable> functions = new LinkedHashMap<>();
 
-        for (Map.Entry<Integer, NamedRunnable> entry : super.getFunctionsMap(startIndex).entrySet()) {
-            functions.put(startIndex++, entry.getValue());
-        }
+		functions.put(startIndex++, new NamedRunnable(this::signRequests, "Sign Request"));
 
-        return functions;
-    }
+		for (Map.Entry<Integer, NamedRunnable> entry : super.getFunctionsMap(startIndex).entrySet()) {
+			functions.put(startIndex++, entry.getValue());
+		}
+
+		return functions;
+	}
 
 }
